58d7e518389961c191bfb86a14d3546435808afc
ajaiisnow <ajaiisnow@gmail.com>
2017-10-03 11:55:38 -0400

    Added

--- BinarySearchTree/BinarySearchTree/quick_sort.cpp
+++ BinarySearchTree/BinarySearchTree/quick_sort.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
 using namespace std;
 
-//void swap(int& a, int &b)
-//{
-//    int sum = a+b;
-//    a = sum - a;
-//    b = sum - b;
-//}
-//
-//void quick_sort(int *_array, int start, int end)
-//{
-//    int pivot = end;
-//    int i = start -1;
-//    int j = start;
-//    while( j < pivot-1)
-//    {
-//        while (_array[j] > _array[pivot])
-//        {
-//            // till you find a value greater than the pivot element 
-//            // increment.
-//            ++j;
-//        }
-//        // else you found a value less than the pivot element and so swap with the first element in the array.
-//        ++i;
-//        swap(_array[i], _array[j]);
-//    }
-//    swap( _array[i+1], _array[pivot]);
-//    quick_sort( _array, start, i-1);
-//    quick_sort( _array, i+1, end);
-//}
+void swap(int& first, int& second)
+{
+    int sum = first + second;
+    first = sum - first;
+    second = sum - second;
+}
+int Partition(int* _array, int start, int end)
+{
+    int i = start - 1;
+    int j = start;
+    int &pivot = _array[end];
+    while( j <= end-1 )
+    {
+        if(_array[j] <= pivot )
+        {
+            swap(_array[++i], _array[j]);
+        }
+        ++j;
+    }
+    swap(_array[++i], pivot);
+    return i;
+
+}
+
+void quick_sort(int* _array, int start, int end)
+{
+    if( start < end)
+    {
+        int partition = Partition(_array, start, end);
+        quick_sort(_array, start, partition-1 );
+        quick_sort(_array, partition + 1, end);
+    }
+ 
+}
